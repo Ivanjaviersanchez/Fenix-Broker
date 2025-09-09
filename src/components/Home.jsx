120e4b3f88698d416525aeb5fb1597a283930ca5
Ivanjaviersanchez <ivanjaviersanchez0108@gmail.com>
2025-09-09 01:26:32 -0300

    Componente Home Preguntas frecuentes 9/9

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,8 @@ import '../colors.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 
+import Accordion from 'react-bootstrap/Accordion';
+
 
 import CarouselImage1 from '../assets/CarruselHome1.jpeg';
 import CarouselImage2 from '../assets/slideGranizo.jpeg';
@@ -21,7 +23,7 @@ import IconoSegurosAdaptados from '../assets/IconoSegurosAdaptados.png';
 import IconoResponsabilidad from '../assets/IconoResponsabilidad.png';
 
 import SegurosHomeAuto from '../assets/SegurosHomeAuto6.png';
-import SegurosHomeHogar from '../assets/SegurosHomeHogar6.png';
+import SegurosHomeHogar from '../assets/SegurosHomeHogar66.png';
 import SegurosComercio from '../assets/SegurosHomeComercio6.png';
 
 import LogoAllianz from '../assets/CarruselLogoAllianz.png';
@@ -241,6 +243,121 @@ function Home() {
         </div>
       </div>
 
+      {/* Preguntas frecuentes */}
+      <div className='containerPreguntas'>
+
+        <h2 className='tituloPreguntas'>Preguntas frecuentes {">>>"}</h2>
+
+        <div className='containerAcordion'>
+          
+          <div className='acordionLeft'>
+            <h3>¿Que debes saber de tu seguro?</h3>
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>¿Que vigencia tiene la póliza del seguro?</Accordion.Header>
+                <Accordion.Body>
+                  La vigencia de la póliza depende del seguro que contrates, suele ser anual pero pueden existir otras opciones. <br />
+                  Te recomendamos prestar atención a la vigencia, los períodos de facturación y la cantidad de cuotas, ya que son variables que inciden en el costo final del seguro.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>¿En qué casos el seguro no cubre?</Accordion.Header>
+                <Accordion.Body>
+                  Hay varios motivos por los que el seguro puede no darte cobertura, los más habituales son: <br />
+                  • Si no pagaste la cuota de tu seguro en tiempo y forma, la póliza queda suspendida, sin cobertura financiera. <br />
+                  • Que la situación esté incluida entre las exclusiones detalladas en el contrato, es decir aquellos riesgos que no están cubiertos. <br />
+                  Te mencionamos algunos ejemplos: En el caso de los seguros de vehículos, los siniestros provocados bajo estado de ebriedad no están cubiertos. Si asegurás tu auto o moto como uso particular y lo usas para trabajar, no está cubierto.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>¿Qué es la franquicia de un seguro?</Accordion.Header>
+                <Accordion.Body>
+                  La franquicia de un seguro es la parte del siniestro que está a cargo del asegurado, superado ese monto, la compañía indemnizará por la diferencia entre la valoración del daño y la franquicia. <br />
+                  Por ejemplo, si un seguro tiene una franquicia de $100.000- y el  siniestro es por un monto mayor, la compañía descontará la franquicia (es decir $100.000.-) e indemnizará al asegurado por el saldo restante.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3">
+                <Accordion.Header>¿Qué significa sin franquicia en un seguro?</Accordion.Header>
+                <Accordion.Body>
+                  Un seguro sin franquicia significa que la compañía de seguros cubre la indemnización completa en caso de un siniestro, sin que el asegurado deba pagar nada adicional.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="4">
+                <Accordion.Header>¿Qué cubre el seguro contra todo riesgo con franquicia?</Accordion.Header>
+                <Accordion.Body>
+                  Te cubre ante responsabilidad civil, pérdida total y parcial por incendio, robo y/o hurto y accidente. Es ideal para quienes  quieren reducir el riesgo al valor de la franquicia. <br />
+                  Con Fenix, en seguros de automotor los daños parciales se cubren de acuerdo a la franquicia que elijas, puede ser franquicia de 3% o de 6% sobre el valor de 0km.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="5">
+                <Accordion.Header>¿Qué cubre el seguro contra terceros? ¿Qué cubre el seguro de responsabilidad civil?</Accordion.Header>
+                <Accordion.Body>
+                  Para los automóviles y motos que circulan, es  la cobertura de contratación obligatoria para cumplir con las normativas vigentes. <br />
+                  Está incluida en todos nuestros planes de seguro de autos y motos. <br />
+                  El seguro de Responsabilidad Civil en general, en cualquier seguro, te cubre frente a reclamos como consecuencia de daños personales y materiales que le generes, con el bien asegurado, a terceras personas.
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <div className='acordionRight'>
+            <h3>Respuestas rapidas para nuestros asegurados</h3>
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Auxilio Mecánico: ¿Cómo solicito la grúa?</Accordion.Header>
+                <Accordion.Body>
+                  Deberás llamar a SOS a los siguientes teléfonos dependiendo donde te encuentres: <br />
+                  Desde Argentina:   0800.345.1240 <br />
+                  Desde otro país    +54 351 4858321 (Cobro revertido) <br />
+                  Cobertura en: Argentina, Chile, Brasil (debajo del paralelo 24, Estado de Santa Catarina), Uruguay, Paraguay, Bolivia y Perú.  Río de Janeiro excluido. <br />
+                  Tené en cuenta que la asistencia para Perú será a través de reintegro, por lo que deberás solicitar el servicio a SOS y luego efectuar el trámite correspondiente.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>¿Cómo pago mi seguro?</Accordion.Header>
+                <Accordion.Body>
+                  Podrás efectuar el pago de la póliza mediante: <br />
+                  • Débito automático en cuenta bancaria por CBU o Tarjeta de Crédito (Visa, MasterCard, American Express, Argencard, Naranja y Titanio). <br />
+                  • En efectivo (cuponera) <br />
+                  Pagos online en Mercado Pago, Pago Mis Cuentas, Provincia NET, RIPSA PAGOS, Pronto Pago. <br />
+                  Pagos  presenciales en Pago Fácil, Rapipago, Cobro Express, RIPSA PAGOS, Pronto Pago.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>¿Qué hacer en caso de siniestro?</Accordion.Header>
+                <Accordion.Body>
+                  En caso de que hayas tenido un siniestro tenés que saber que hay un plazo de 3 días para hacer la denuncia. <br />
+                  Podés realizar la denuncia a través de alguno de nuestros canales: <br />
+                  • A través de tu Productor Asesor de Seguros. <br />
+                  • APP de Fenix Seguros <br />
+                  • Portal de asegurados de Fenix Seguros <br />
+                  • Por email a denuncias@-------.com.ar. En el asunto deberás colocar: DNI, nombre y apellido; y denuncia de (tipo de seguro) <br />
+                  • Equipo de Atención al Cliente: Llamando al 0810------- de lunes a viernes de 9 a 19 horas.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3">
+                <Accordion.Header>¿Cómo accedo a mi póliza digital en el celular?</Accordion.Header>
+                <Accordion.Body>
+                  Como asegurado podés visualizar tu seguro en la APP Mi Argentina. <br />
+                  Como asegurado de Fenix, podés descargar la App móvil o ingresar al portal del asegurado y bajar tu póliza, tarjeta de circulación, certificado de cobertura y de Mercosur, entre otros documentos que necesites. <br />
+                  • APP de Fenix Seguros <br />
+                  • Portal de asegurados de Fenix Seguros
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="4">
+                <Accordion.Header>Baja del seguro: ¿Querés dar de baja la póliza?</Accordion.Header>
+                <Accordion.Body>
+                  En el caso de que decidas anular tu póliza vigente podés hacerlo en cualquier momento. Para eso deberás solicitarlo a: <br />
+                  • Tu Productor Asesor de Seguros. <br />
+                  • Equipo de Atención al Cliente: De lunes a viernes de 9 a 19 horas al 0810-345-0492 <br />
+                  • Portal de asegurados de Fenix Seguros <br />
+                  • APP de Fenix Seguros
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          
+        </div>
+      </div>
 
       {/* Redes sociales */}
       <div className='containerRedesHome'>
